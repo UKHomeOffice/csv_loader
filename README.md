e2f90ab13473cf764ca2ab67ca6112faa729b708
Ivan Gavryliuk <aloneguid@outlook.com>
2021-05-21 11:05:08 +0100

    fix reference to "digitalpatterns" (#2)
    * checkpoint
    
    * - remove reference to "digitalpatterns" docker repo
    - experimental management interface
    - documentation improvements

--- README.md
+++ README.md
@@ -1,106 +1,20 @@
-# Reference data governance service
+# CSV Loader
 
-## Database schema design
-
-Each Table must contain a comment in JSON format containing the following entities:
-
-* description
-* schemalastupdated
-* dataversion
-* `owner` - a group email like `owner@example.com`
-
-and these columns are required:
-
-* validfrom
-* validto
-
-
-Each Column must contain a comment in JSON format containing the following entities:
-
-* label
-* description
-
-One column must also contain the comment:
-
-* businesskey: "true"
-
-Optional entities for column comments:
-
-* aliases (comma separated list)
-* minimumlength
-* maximumlength
-* minimumvalue
-* maximumvalue
+This is a helper repository showing how to manage data schemas and data itself and set up services for managing them.
 
 *There should be NO CSVs or Schemas checked into this repo*, the ones here are samples. It is recommended that schemas
 and csvs should be put into separate repos. This docker image can then be used as a base image or with the 
 docker-compose.general.yml to override the locations.
 
-## Grants
+# Setting Up
 
-Each table must have at least two GRANTs, as follows:
-* serviceuser - This used by Camunda to allow new records to be added to the Reference data and current records to be updated.
-* readonlyuser - This user can read all records.
-* anonuser - This user should only be added to public tables where authentication is not needed to see the data.
-  This should be the default for all datasets unless they are deemed sensitive.
+## Prerequisites
 
+- Docker & Docker Compose (on Linux, you may need to [update Compose separately](https://docs.docker.com/compose/install/#install-compose)).
 
-### Example
+### Required dependencies (Ubuntu)
 
-```sql
-CREATE TABLE ministry (
-  id INTEGER NOT NULL PRIMARY KEY,
-  name VARCHAR(60) NOT NULL,
-  code VARCHAR(8) NOT NULL,
-  validfrom TIMESTAMP WITH TIME ZONE,
-  validto TIMESTAMP WITH TIME ZONE,
-  updatedby VARCHAR(60) NULL
-);
-
--- Table comment
-COMMENT ON TABLE ministry IS '{"label": "Government ministries", "owner": "xyx@test.com", "description": "A list of departments, agencies and public bodies.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
--- Column comments
-COMMENT ON COLUMN ministry.id IS '{"label": "Identifier", "description": "Database unique identity record."}';
-COMMENT ON COLUMN ministry.name IS '{"label": "Name", "description": "The name of the branch or region."}';
-COMMENT ON COLUMN ministry.code IS '{"label": "Code", "businesskey": true, "description": "The code associated with the branch or region."}';
-COMMENT ON COLUMN ministry.validfrom IS '{"label": "Valid from date", "description": "Item valid from date."}';
-COMMENT ON COLUMN ministry.validto IS '{"label": "Valid to date", "description": "Item valid to date."}';
-COMMENT ON COLUMN ministry.updatedby IS '{"label": "Updated By", "description": "Record updated by"}';
-
--- GRANTs
-GRANT SELECT,INSERT,UPDATE ON ministry TO ${serviceuser};
-GRANT SELECT ON ministry TO ${readonlyuser};
-GRANT SELECT ON ministry TO ${anonuser};
-```
-
-
-
-## Environment variables
-
-* DB_HOSTNAME
-* DB_PORT
-* DB_NAME
-* DB_DEFAULT_NAME
-* DB_OPTIONS
-* DB_JDBC_OPTIONS
-* FLYWAY_USER
-* FLYWAY_PASSWORD
-* DB_OWNERNAME
-* DB_OWNERPASSWORD
-* DB_SCHEMA
-* FLYWAY_PLACEHOLDERS_AUTHENTICATORUSER
-* FLYWAY_PLACEHOLDERS_AUTHENTICATORPASSWORD
-* FLYWAY_PLACEHOLDERS_ANONUSER
-* FLYWAY_PLACEHOLDERS_SERVICEUSER
-* FLYWAY_PLACEHOLDERS_READONLYUSER
-
-## Development
-
-### Setting Up
-
-#### Required dependencies (Ubuntu)
-
-```
+```bash
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 sudo apt update
@@ -115,23 +29,41 @@ sudo apt install git vim jq curl postgresql-client-common postgresql-client-12 g
 
 - [Get Postman](https://www.postman.com/downloads/)
 
+# Schema and Data Prerequisites
 
+If you follow this guide, keep in mind that `docker-compose.general.yml` expects the following directory structure on your local disk:
+
+- `flyway` - schema definitions. There is a folder in current repository with sample schema. Most probably you would clone [RefData Schema]([UKHomeOffice/RefData: Reference data and schema for RefData project stream (github.com)](https://github.com/UKHomeOffice/RefData)) as `flyway` folder (`git clone git@github.com:UKHomeOffice/RefData.git flyway`).
+- `csv` - data scripts. Current repo's `csvs` folder contains a sample of those. You will most likely clone [csv repo]([cop / csv · GitLab (digital.homeoffice.gov.uk)](https://gitlab.digital.homeoffice.gov.uk/cop/csv))  as `csv`.
+- `csv_loader` - **this repo**. 
+
+![](media/folders.png)
+
+Note the picture above - csv_loader (this repo, #3) contains "flyway" and "csvs" as subfolders, but those are not the folders you need. #1 and #2 have to be cloned on the same directory level as "csv_loader".
 
 ## Start up Docker
 
 
 ### Stage 1
 
-Start the supporting services.
+Start Postgres database:
 
 ```bash
-docker-compose -f docker-compose.yml -f docker-compose.general.yml up -d db keycloak
+docker-compose -f docker-compose.yml -f docker-compose.general.yml up -d db
 ```
 
+Start Keycloak:
+
+```bash
+docker-compose -f docker-compose.yml -f docker-compose.general.yml up -d keycloak
+```
+
+No that keycloak is a heavy piece of software and it may take a couple of minutes for it to become available.
 
 Go to the following url [http://localhost:8080/auth/admin/master/console/#/realms/rocks/users](http://localhost:8080/auth/admin/master/console/#/realms/rocks/users) 
 Login as admin/admin.
 Create a new user called demo with the following settings:
+
 * username: demo
 * email: demo@localhost
 * first name: demo
---
+++ mgmt.py
@@ -0,0 +1,218 @@
+from dearpygui.core import *
+from dearpygui.simple import *
+from dearpygui.demo import show_demo
+import docker
+import threading
+import time
+import webbrowser
+import requests
+import json
+import os
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+# noinspection PyUnresolvedReferences
+from psycopg2.errors import DuplicateDatabase, DuplicateObject, DuplicateTable
+
+running = True
+watched_containers = []
+windows = set()
+container_name_to_id = {}
+USE_COMPOSE = True
+COMPOSE_FILE_NAME = "./docker-compose.general.yml"
+
+
+def start_container(sender, name):
+    log_info(f"starting container {name}")
+    if USE_COMPOSE:
+        os.system(f"docker-compose -f docker-compose.yml -f docker-compose.general.yml up -d {name}")
+    else:
+        id = container_name_to_id[name]
+        container = dc.containers.get(id)
+        container.start()
+
+
+def stop_container(sender, name):
+    log_info(f"stopping container {name}")
+    if USE_COMPOSE:
+        os.system(f"docker-compose stop {name}")
+    else:
+        id = container_name_to_id[name]
+        container = dc.containers.get(id)
+        container.stop()
+
+
+def view_container_logs(sender, name):
+    rf_name = f"{name}#log#refresh"
+    tx_name = f"{name}#log#text"
+
+    def refresh_logs(sender1, data1):
+        log_info(f"fetching logs for container {name}")
+        id = container_name_to_id[name]
+        container = dc.containers.get(id)
+        lines = container.logs()  # bytes
+        tx_lines = lines.decode("utf-8")
+        log_info(f"setting for {rf_name}: {len(tx_lines)} char(s).")
+        set_value(tx_name, tx_lines)
+
+    window_name = f"{name}#logs#window"
+
+    if window_name not in get_windows():
+        with window(window_name, label=f"Logs :: {name}", width=610, height=400):
+            add_button(rf_name, label="refresh", callback=refresh_logs, callback_data=name)
+            add_input_text(tx_name, multiline=True, label="", width=600, height=340)
+
+    refresh_logs(None, rf_name)
+    show_item(window_name)
+
+
+def add_status_report(name: str):
+    # add_text(f"{name}#status")
+    watched_containers.append(name)
+    lbl_name = f"{name}#status"
+    add_label_text(lbl_name, label="unknown")
+    set_item_width(lbl_name, 80)
+
+    add_button(f"{name}#start", label="start", callback=start_container, callback_data=name)
+    add_same_line()
+    add_button(f"{name}#stop", label="stop", callback=stop_container, callback_data=name)
+    add_same_line()
+    add_button(f"{name}#logs", label="logs", callback=view_container_logs, callback_data=name)
+
+
+def set_status_report(name: str, status: str):
+    ename = f"{name}#status"
+    # set_value(ename, status)
+    set_item_label(ename, status.upper())
+
+
+def get_jwt(sender, data):
+    key = requests.get("http://localhost:8080/auth/realms/rocks/protocol/openid-connect/certs").json()["keys"][0]
+    key = {
+        "kid": key["kid"],
+        "kty": key["kty"],
+        "alg": key["alg"],
+        "n": key["n"],
+        "e": key["e"]
+    }
+    j_key = json.dumps(key)
+    set_value("keycloak#jwt", j_key)
+
+    # update yaml
+    import yaml
+    with open(COMPOSE_FILE_NAME) as f:
+        config = yaml.full_load(f)
+        config["services"]["postgrest"]["environment"] = [f"PGRST_JWT_SECRET={j_key}"]
+
+    with open(COMPOSE_FILE_NAME, "w") as f:
+        f.write(yaml.dump(config))
+
+
+
+def refresh_table_list(sender, data):
+    # reconnect to the reference database
+    conn = psycopg2.connect(host="db", database="ref", user="postgres", password="postgres")
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+
+
+def drop_db(sender, data):
+    conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="postgres")
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute("drop database ref;")
+    cur.execute("drop user anonuser,authuser,dbowner,readonlyuser,serviceuser;")
+    cur.execute("drop table flyway_schema_history;")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+with window("window#schema", label="Schema Browser", width=600):
+    add_combo("schema#tables", label="table")
+    add_same_line()
+    add_button("schema#tables#refresh", label="refresh")
+
+
+def open_schema_browser(sender, data):
+    show_item("window#schema")
+
+
+with window("Main"):
+
+    with menu_bar("menu#main"):
+        with menu("menu#db", label="DB"):
+            add_menu_item("menu#db#clean", label="Drop", callback=drop_db)
+            add_menu_item("menu#db#schema", label="Schema Browser", callback=open_schema_browser)
+            add_menu_item("menu#db#view", label="Data Viewer")
+
+    with node_editor("servers"):
+        with node("db", label="DB", x_pos=30, y_pos=80):
+            with node_attribute("db#out", output=True):
+                add_status_report("db")
+
+        with node("flyway", label="Schema Loader", x_pos=260, y_pos=10):
+            with node_attribute("flyway#in"):
+                add_status_report("flyway")
+            with node_attribute("flyway#out", output=True):
+                pass
+
+        with node("validate", label="Schema Validator", x_pos=450, y_pos=80):
+            with node_attribute("validate#in"):
+                add_status_report("validate")
+
+        with node("keycloak", label="Keycloak", x_pos=200, y_pos=250):
+            with node_attribute("keycloak#in"):
+                add_status_report("keycloak")
+                add_button("keycloak#browser", label="users",
+                           callback=lambda s, d: webbrowser.open(
+                               "http://localhost:8080/auth/admin/master/console/#/realms/rocks/users"))
+                add_button("keycloak#getjwt", label="get JWT token",
+                           callback=get_jwt)
+                add_input_text("keycloak#jwt", width=200, label="")
+            with node_attribute("keycloak#out", output=True):
+                pass
+
+        with node("postgrest", label="PostgREST", x_pos=500, y_pos=220):
+            with node_attribute("postgrest#in"):
+                add_status_report("postgrest")
+            with node_attribute("postgrest#out", output=True):
+                pass
+
+        with node("csv_loader", label="CSV Loader", x_pos=700, y_pos=350):
+            with node_attribute("csv_loader#in"):
+                add_status_report("csv_loader")
+
+    add_node_link("servers", "db#out", "flyway#in")
+    add_node_link("servers", "db#out", "validate#in")
+    add_node_link("servers", "flyway#out", "validate#in")
+
+    add_node_link("servers", "db#out", "keycloak#in")
+    add_node_link("servers", "keycloak#out", "postgrest#in")
+    add_node_link("servers", "db#out", "postgrest#in")
+
+    add_node_link("servers", "keycloak#out", "csv_loader#in")
+    add_node_link("servers", "postgrest#out", "csv_loader#in")
+
+set_main_window_title("Ref Data")
+
+dc = docker.from_env()
+
+
+def background():
+    while running:
+        containers = dc.containers.list(all=True)
+        for c in containers:
+            container_name_to_id[c.name] = c.id
+            if c.name not in watched_containers:
+                continue
+            set_status_report(c.name, c.status)
+
+        time.sleep(2)
+
+
+# show_logger()
+# show_demo()
+bg = threading.Thread(name="background", target=background)
+bg.start()
+start_dearpygui(primary_window="Main")
+running = False
